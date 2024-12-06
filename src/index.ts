d4f6ace7af51dd1a9dcf80b660eee5e2c9939ede
sachin <sachinb0841@gmail.com>
2024-12-06 10:17:11 +0530

    dist added

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cors from "cors"
 import router from "./routes/todo.routes";
 import authrouter from "./routes/auth.routes";
 import cookieparser from "cookie-parser"
+import path from "path"
 dotenv.config();
 const app = express()
 const MONGO_URL: string = process.env.MONGO_URL || ""
@@ -12,11 +13,12 @@ const PORT: number = parseInt(process.env.PORT || "5000", 10);
 
 app.use(express.json())
 app.use(cookieparser())
+app.use(express.static(path.join(__dirname, "src", "dist")));
 app.use(cors({ origin: true, credentials: true }))
 app.use("/api", router)
 app.use("/api/auth", authrouter)
-app.get('/test', (req: Request, res: Response): void => {
-    res.json({ data: "test page" });
+app.use('*', (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, 'src', 'dist', 'index.html'));
 });
 
 mongoose.connect(MONGO_URL)
